a3ad09dda5ec83d7fb6f872a162683dea39e6786
Teknix <pandapaloo@gmail.com>
2021-01-19 16:37:20 -0700

    Changed the Footer so that the links open in new tabs

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,10 +8,10 @@ function Footer() {
     return (
         <footer>
             <div className="container">
-                <a className="footerLink" href="https://www.facebook.com/Teknix420"><img src={Facebook} alt="Facebook" height="32px" /></a>
-                <a className="footerLink" href="https://www.linkedin.com/in/travis-gray-0259b1a6/"><img src={LinkedIn} alt="LinkedIn" height="32px" /></a>
-                <a className="footerLink" href="mailto:TravisGray420@gmail.com"><img src={Gmail} alt="Email" height="32px" /></a>
-                <a className="footerLink" href="https://www.github.com/Teknix420"><img src={Github} alt="Github" height="32px" /></a>
+                <a target="_blank" rel="noopener noreferrer" className="footerLink" href="https://www.facebook.com/Teknix420"><img src={Facebook} alt="Facebook" height="32px" /></a>
+                <a target="_blank" rel="noopener noreferrer" className="footerLink" href="https://www.linkedin.com/in/travis-gray-0259b1a6/"><img src={LinkedIn} alt="LinkedIn" height="32px" /></a>
+                <a target="_blank" rel="noopener noreferrer" className="footerLink" href="mailto:TravisGray420@gmail.com"><img src={Gmail} alt="Email" height="32px" /></a>
+                <a target="_blank" rel="noopener noreferrer" className="footerLink" href="https://www.github.com/Teknix420"><img src={Github} alt="Github" height="32px" /></a>
             </div>
         </footer>
     )
